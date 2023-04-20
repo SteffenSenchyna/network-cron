dc3a9da06ccca6df668705e88ef8954f77815886
SteffenSenchyna <105737839+SteffenSenchyna@users.noreply.github.com>
2023-04-19 22:22:37 -0600

    Updated file structure for cronjobs

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,12 @@ FROM python:3.9.2-alpine3.13
 RUN apk update && apk add bash
 RUN apk add gcc musl-dev && \
     pip install --upgrade setuptools
+
+COPY jobs   /app
+COPY requirements.txt /app
+COPY mibs /usr/local/lib/python3.9/site-packages/pysnmp/smi/mibs/
 WORKDIR /app
-COPY . /app
-COPY ./mibs/ /usr/local/lib/python3.9/site-packages/pysnmp/smi/mibs
 RUN pip install -r requirements.txt 
+
+
 ENTRYPOINT ["/bin/sh"]
--- docker-build.py
+++ docker-build.py
@@ -3,8 +3,8 @@ import os
 # Define the image name and tag
 image_name = 'network'
 image_tag = 'Test'
-os.system(f"docker image prune -y")
+os.system(f"docker image prune")
 os.system(
     f'docker build -t {image_name}:{image_tag} .')
 os.system(
-    f'docker run --rm -it --entrypoint /bin/sh {image_name}:{image_tag}')
+    f'docker run --rm -it --env-file .env --network bridge --entrypoint /bin/sh {image_name}:{image_tag}')
